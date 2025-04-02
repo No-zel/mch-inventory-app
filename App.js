59549c8a88859dd2b3b6a9e7790983b3d953aa8a
Zelly <veriousbus@gmail.com>
2025-04-03 02:24:29 +0800

    reset

--- App.js
+++ App.js
@@ -1,17 +1,47 @@
-import { NavigationContainer } from "@react-navigation/native";
 import React from "react";
-import AuthLayout from "./layout/AuthLayout";
-import AllRoutes from "./routes/all_routes";
-import { RootSiblingParent } from 'react-native-root-siblings';
+import { View, Text, StyleSheet } from "react-native";
 
 export default function App() {
   return (
-      <NavigationContainer>
-        <AuthLayout>
-          <RootSiblingParent>
-            <AllRoutes />
-          </RootSiblingParent>
-        </AuthLayout>
-      </NavigationContainer>
+    <View style={styles.container}>
+      <Text style={styles.text}>yes🚀</Text>
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#f0f0f0",
+  },
+  text: {
+    fontSize: 20,
+    fontWeight: "bold",
+    color: "#333",
+  },
+});
+// import { NavigationContainer } from "@react-navigation/native";
+// import React from "react";
+// // import AuthLayout from "./src/layout/AuthLayout";
+// // import AllRoutes from "./src/routes/all_routes";
+// import { RootSiblingParent } from 'react-native-root-siblings';
+
+// export default function App() {
+//   return (
+//     <NavigationContainer>
+//         <RootSiblingParent>
+//         </RootSiblingParent>
+//     </NavigationContainer>
+// );
+//   // return (
+//   //     <NavigationContainer>
+//   //       <AuthLayout>
+//   //         <RootSiblingParent>
+//   //           {/* <AllRoutes /> */}
+//   //         </RootSiblingParent>
+//   //       </AuthLayout>
+//   //     </NavigationContainer>
+//   // );
+// }
